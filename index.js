9d305a8f850bef284abe3bb390a6ae542fd75b79
Shams Shimul <msr.shimul@gmail.com>
2024-12-16 12:35:58 +0600

    Challenge completed
    - The main goal and all stretch goals achieved

--- index.js
+++ index.js
@@ -1,14 +1,37 @@
-const snowGlobe = document.querySelector('.snow-globe')
-
+const snowGlobe = document.querySelector(".snow-globe");
+const snowGlobeWithBase = document.querySelector("main");
+let count = 0;
+const fontSizes = ["12px", "16px", "20px", "24px"];
 function createSnowflake() {
-/* 
-Challenge:
-1. Write JavaScript to create a snowflake and make it fall inside the snow globe. The snowflake should have a random starting position, animation duration, and size.
-2. See index.css
-*/ 
+  /* 
+  Challenge:
+  1. Write JavaScript to create a snowflake and make it fall inside the snow globe. The snowflake should have a random starting position, animation duration, and size.
+  2. See index.css
+  */
+  ++count;
+
+  const snowflake = document.createElement("span");
+  snowflake.classList.add("snowflake");
+  snowflake.innerHTML = count % 25 === 0 ? "☃️" : "❄️";
+
+  const posX = Math.floor(
+    Math.random() * (snowGlobe.getBoundingClientRect().width + 100)
+  );
+  snowflake.style.left = `${posX - 150}px`;
+  snowflake.style.fontSize = fontSizes[Math.floor(Math.random() * 4)];
+
+  const duration = (Math.floor(Math.random() * 4) + 2) * 1000; //between 2s and 5s (both inclusive)
+  snowflake.style.animationDuration = `${duration}ms`;
+
+  snowGlobe.appendChild(snowflake);
+
+  // removing the snowflake after the animation duration ends
+  setTimeout(() => {
+    snowflake.remove();
+  }, duration);
 }
 
-setInterval(createSnowflake, 100) // Let's create a snowflake every 100 milliseconds!
+// setInterval(createSnowflake, 100) // Let's create a snowflake every 100 milliseconds!
 
 /* Stretch goals: 
 - Give some variety to your snowflakes, so they are not all the same. Perhaps every 25th one could be a snowman ☃️?
@@ -17,3 +40,21 @@ setInterval(createSnowflake, 100) // Let's create a snowflake every 100 millisec
 - Change the direction of the snowflakes so they don’t all fall vertically.
 - Make the style your own! 
 */
+const button = document.querySelector("button");
+const buttonBG = document.querySelector("button span");
+document.querySelector("button").addEventListener("click", e => {
+  console.log("EVENT HANDLER TRIGGERED");
+  const intervalID = setInterval(createSnowflake, 100);
+
+  snowGlobeWithBase.classList.add("shake"); // shake effect
+  button.disabled = true;
+  buttonBG.classList.add("regain");
+
+  // After 20s, stop the snowflake animation
+  setTimeout(() => {
+    clearInterval(intervalID);
+    button.disabled = false;
+    snowGlobeWithBase.classList.remove("shake");
+    buttonBG.classList.remove("regain");
+  }, 20000);
+});
